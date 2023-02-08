06776b661aff6c310009a6262930cabfa1fdd9e4
Brent Rahn <brent.rahn@gmail.com>
2023-02-07 20:42:47 -0800

    initial commit

---
+++ dungeonbot.go
@@ -0,0 +1,388 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	_ "image/jpeg"
+	"image/png"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+
+	cwmaze "dungeonbot/maze"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type TGBot struct {
+	API_KEY string
+}
+
+func (t TGBot) DownloadFile(filePath string) (*http.Response, error) {
+	url := "https://api.telegram.org/file/bot" + t.API_KEY + "/" + filePath
+	fmt.Println("Downloading", url)
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode != http.StatusOK {
+		return nil, errors.New("unexpected status" + res.Status)
+	}
+	return res, nil
+}
+
+func (t TGBot) SendCommand(cmd string, body interface{}) (*http.Response, error) {
+	// Create the JSON body from the struct
+	reqBytes, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Send a post request with your token
+	url := "https://api.telegram.org/bot" + t.API_KEY + "/" + cmd
+	res, err := http.Post(url, "application/json", bytes.NewBuffer(reqBytes))
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode != http.StatusOK {
+		return nil, errors.New("unexpected status " + res.Status)
+	}
+	return res, nil
+}
+
+func (t TGBot) Respond(m TGMessage, s string) (*http.Response, error) {
+	return t.SendCommand("sendMessage", struct {
+		ChatID int64  `json:"chat_id"`
+		Text   string `json:"text"`
+	}{m.Chat.ID, s})
+}
+
+func (t TGBot) SetWebhook(url string) (*http.Response, error) {
+	return t.SendCommand("setWebhook", struct {
+		Url string `json:"url"`
+	}{url})
+}
+
+type SendFile struct {
+	field    string
+	filename string
+	reader   io.Reader
+}
+
+func (t TGBot) SendFiles(cmd string, body interface{}, files []SendFile) (*http.Response, error) {
+	r, w := io.Pipe()
+	m := multipart.NewWriter(w)
+
+	url := "https://api.telegram.org/bot" + t.API_KEY + "/" + cmd
+
+	go func() {
+		defer w.Close()
+		defer m.Close()
+
+		rv := reflect.ValueOf(body)
+		t := rv.Type()
+		for i := range reflect.VisibleFields(t) {
+			name, _ := t.Field(i).Tag.Lookup("json") // Check for errors?
+			value := ""
+			switch rv.Field(i).Kind() {
+			case reflect.Int:
+				value = strconv.FormatInt(rv.Field(i).Int(), 10)
+			case reflect.Int64:
+				value = strconv.FormatInt(rv.Field(i).Int(), 10)
+			case reflect.String:
+				value = rv.Field(i).String()
+			case reflect.Bool:
+				value = strconv.FormatBool(rv.Field(i).Bool())
+			}
+			fmt.Println("encoding field: ", name, value)
+			_ = m.WriteField(name, value)
+		}
+
+		for i := range files {
+			part, err := m.CreateFormFile(files[i].field, files[i].filename)
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+			io.Copy(part, files[i].reader)
+		}
+
+	}()
+
+	req, _ := http.NewRequest("POST", url, r) // as you can see I have passed the pipe reader here
+	req.Header.Set("Content-Type", m.FormDataContentType())
+	res, err := http.DefaultClient.Do(req) // do the request. The program will stop here until the upload is done
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	/*
+		data, _ := io.ReadAll(resp.Body) // read the results
+		fmt.Println(string(data))
+	*/
+	return res, nil
+}
+
+type TGFile struct {
+	FileID   string `json:"file_id"`
+	FilePath string `json:"file_path"`
+}
+
+type TGPhotoSize struct {
+	FileID string `json:"file_id"`
+	Width  int    `json:"width"`
+	Height int    `json:"height"`
+}
+
+type TGMessage struct {
+	Text string `json:"text"`
+	ID   int64  `json:"message_id"`
+	From struct {
+		ID       int64  `json:"id"`
+		Username string `json:"username"`
+	} `json:"from"`
+	Chat struct {
+		ID int64 `json:"id"`
+	} `json:"chat"`
+	Photo []TGPhotoSize `json:"photo"`
+}
+
+type TGResponse[T any] struct {
+	Ok     bool `json:"ok"`
+	Result T    `json:"result"`
+}
+
+// Create a struct that mimics the webhook response body
+// https://core.telegram.org/bots/api#update
+type webhookReqBody struct {
+	Message TGMessage `json:"message"`
+}
+
+func getFullSizeImage(photos []TGPhotoSize) string {
+	largest := 0
+	var fullSizeImage *TGPhotoSize
+
+	for i := range photos {
+		if photos[i].Width > largest {
+			fullSizeImage = &photos[i]
+			largest = photos[i].Width
+		}
+	}
+
+	return fullSizeImage.FileID
+}
+
+// This handler is called everytime telegram sends us a webhook event
+func Handler(res http.ResponseWriter, req *http.Request) {
+	// First, decode the JSON response body
+	body := &webhookReqBody{}
+
+	if err := json.NewDecoder(req.Body).Decode(body); err != nil {
+		fmt.Println("could not decode request body", err)
+		return
+	}
+
+	if body.Message.Photo != nil {
+		fullSizeImage := getFullSizeImage(body.Message.Photo)
+		res, err := bot.SendCommand("getFile", struct {
+			FileID string `json:"file_id"`
+		}{fullSizeImage})
+		if err != nil {
+			bot.Respond(body.Message, "Failed to get map image from Telegram server")
+			fmt.Println(err)
+			return
+		}
+
+		fileInfo := &TGResponse[TGFile]{}
+
+		if err := json.NewDecoder(res.Body).Decode(fileInfo); err != nil {
+			bot.Respond(body.Message, "Failed to get map image from Telegram server")
+			fmt.Println("could not decode getFile response", err)
+			return
+		}
+
+		fileRes, err := bot.DownloadFile(fileInfo.Result.FilePath)
+		if err != nil {
+			bot.Respond(body.Message, "Failed to get map image from Telegram server")
+			fmt.Println(err)
+			return
+		}
+
+		m := cwmaze.Maze{}
+		mazeImage, _, err := image.Decode(fileRes.Body)
+		if err != nil {
+			bot.Respond(body.Message, "Failed to decode map image")
+			fmt.Println(err)
+			return
+		}
+
+		m.Load(mazeImage)
+		bot.Respond(body.Message, m.String())
+
+		buf := new(bytes.Buffer)
+		png.Encode(buf, m)
+
+		sendMaze := []SendFile{
+			{
+				"photo",
+				"maze.png",
+				buf,
+			},
+		}
+		bot.SendFiles("sendPhoto", struct {
+			ChatID int64 `json:"chat_id"`
+		}{body.Message.Chat.ID}, sendMaze)
+
+		mazeJson, err := json.Marshal(m)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		err = redisClient.Set(ctx, fmt.Sprint(body.Message.Chat.ID), mazeJson, 0).Err()
+		if err != nil {
+			fmt.Println(err)
+			bot.Respond(body.Message, "Failed to save map")
+		}
+
+	} else if strings.Contains(body.Message.Text, "You stopped and tried to mark your way on paper.") {
+
+		sections := strings.Split(body.Message.Text, "\n\n")
+
+		maze := &cwmaze.Maze{}
+		mazeJson, err := redisClient.Get(ctx, fmt.Sprint(body.Message.Chat.ID)).Result()
+		if err != nil {
+			if err == redis.Nil {
+				bot.Respond(body.Message, "No map found, please forward map before sending a scribble.")
+			} else {
+				bot.Respond(body.Message, "Error fetching map")
+				fmt.Println("error fetching map from redis", err)
+			}
+			return
+		}
+
+		if err := json.NewDecoder(strings.NewReader(mazeJson)).Decode(maze); err != nil {
+			fmt.Println("could not decode request body", err)
+			return
+		}
+
+		if len(sections) > 1 {
+			matches := maze.SearchByScribble(sections[1])
+
+			fmt.Println(matches)
+
+			bot.Respond(body.Message, matches.String())
+
+			/* This will send a picture of the scribble
+			buf := new(bytes.Buffer)
+			png.Encode(buf, matches)
+
+			sendPic := []SendFile{
+				{
+					"photo",
+					"scribble.png",
+					buf,
+				},
+			}
+			bot.SendFiles("sendPhoto", struct {
+				ChatID int64 `json:"chat_id"`
+			}{body.Message.Chat.ID}, sendPic)
+			*/
+			// create the composite image with the map and scribbles highlighted
+			composite := image.NewRGBA(image.Rect(0, 0, maze.Bounds().Dx(), maze.Bounds().Dy()))
+			draw.Draw(composite, composite.Bounds(), maze, maze.Bounds().Bounds().Min, draw.Src)
+			for _, match := range matches.Matches {
+				drawRect(composite, color.RGBA{255, 25, 25, 255},
+					match.X*5, match.Y*5,
+					match.X*5+matches.Bounds().Dx(), match.Y*5+matches.Bounds().Dy())
+			}
+
+			buf := new(bytes.Buffer)
+			png.Encode(buf, composite)
+
+			sendPic := []SendFile{
+				{
+					"photo",
+					"matches.png",
+					buf,
+				},
+			}
+			bot.SendFiles("sendPhoto", struct {
+				ChatID int64 `json:"chat_id"`
+			}{body.Message.Chat.ID}, sendPic)
+
+		} else {
+			bot.Respond(body.Message, "There seems to be a problem with your scribble")
+		}
+
+	} else {
+		bot.Respond(body.Message, "Try forwarding a map or scribble of a dungeon")
+	}
+
+	// log a confirmation message if the message is sent successfully
+	fmt.Println("reply sent")
+}
+
+// HLine draws a horizontal line
+func drawHLine(img *image.RGBA, col color.Color, x1, y, x2 int) {
+	for ; x1 <= x2; x1++ {
+		img.Set(x1, y, col)
+		img.Set(x1, y-1, col)
+	}
+}
+
+// VLine draws a veritcal line
+func drawVLine(img *image.RGBA, col color.Color, x, y1, y2 int) {
+	for ; y1 <= y2; y1++ {
+		img.Set(x, y1, col)
+		img.Set(x-1, y1, col)
+	}
+}
+
+// Rect draws a rectangle utilizing HLine() and VLine()
+func drawRect(img *image.RGBA, col color.Color, x1, y1, x2, y2 int) {
+	drawHLine(img, col, x1, y1, x2)
+	drawHLine(img, col, x1, y2, x2)
+	drawVLine(img, col, x1, y1, y2)
+	drawVLine(img, col, x2, y1, y2)
+}
+
+func getEnv(key string, fallback string) string {
+	if value, found := os.LookupEnv(key); found {
+		return value
+	}
+	return fallback
+}
+
+var redisClient *redis.Client
+var bot TGBot
+var ctx = context.Background()
+
+func main() {
+	port := getEnv("PORT", "3000")
+
+	redisUrl := getEnv("REDIS_URL", "redis://localhost:6379")
+	opt, _ := redis.ParseURL(redisUrl)
+	redisClient = redis.NewClient(opt)
+
+	pong, err := redisClient.Ping(context.Background()).Result()
+	fmt.Println(pong, err)
+
+	bot = TGBot{API_KEY: getEnv("TG_API_KEY", "abcd:1234")}
+	_, err = bot.SetWebhook("https://happydungeon.fly.dev/" + getEnv("TG_WEBHOOK", ""))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	http.ListenAndServe(":"+port, http.HandlerFunc(Handler))
+}
---
+++ maze/maze.go
@@ -0,0 +1,239 @@
+package cwmaze
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"math"
+	"strings"
+	"unicode"
+)
+
+const (
+	tWALL     = 0
+	tPATH     = 1
+	tFAMOUS   = 2
+	tFOUNTAIN = 3
+	tCHEST    = 4
+	tBONFIRE  = 5
+	tMONSTER  = 6
+	tBOSS     = 7
+	tTEST     = 10
+)
+
+// Represents a Maze, call Load with an image to initialize
+type Maze struct {
+	Pixels [][]uint8     `json:"pixels"`
+	Types  map[uint8]int `json:"types"`
+}
+
+func detectPixelType(img image.Image, rect image.Rectangle) uint8 {
+	red, green, blue := 0.0, 0.0, 0.0
+	for y := rect.Bounds().Min.Y; y < rect.Bounds().Max.Y; y++ {
+		for x := rect.Bounds().Min.X; x < rect.Bounds().Max.X; x++ {
+			c := img.At(x, y)
+			col := color.RGBAModel.Convert(c).(color.RGBA)
+			red += float64(col.R)
+			green += float64(col.G)
+			blue += float64(col.B)
+			//fmt.Println(x, y, red, green, blue)
+		}
+	}
+
+	rectArea := float64(rect.Dx() * rect.Dy())
+	//fmt.Println(rectArea)
+
+	red = math.Floor(red / rectArea)
+	green = math.Floor(green / rectArea)
+	blue = math.Floor(blue / rectArea)
+	if red < 50 && green < 50 && blue < 50 {
+		return tWALL
+	} else if red > 200 && green > 200 && blue > 200 {
+		return tPATH
+	} else if red > 200 && green > 150 {
+		return tBONFIRE
+	} else if red > 200 {
+		return tBOSS
+	} else if green > 200 && blue > 100 {
+		return tCHEST
+	} else if green > 160 {
+		return tFOUNTAIN
+	} else if blue > 200 {
+		if red > 100 && green > 100 {
+			return tMONSTER
+		} else {
+			return tFAMOUS
+		}
+	} else {
+		fmt.Println("Unknown color combination: ", red, green, blue)
+		return 9
+	}
+}
+
+// Initialize a Maze, image should come from Chat Wars
+func (m *Maze) Load(img image.Image) {
+	m.Types = make(map[uint8]int)
+	m.Pixels = make([][]uint8, img.Bounds().Max.Y/5)
+	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y += 5 {
+		m.Pixels[y/5] = make([]uint8, img.Bounds().Max.X/5)
+		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x += 5 {
+			p := detectPixelType(img, image.Rect(x+1, y+1, x+4, y+4))
+			m.Pixels[y/5][x/5] = p
+			m.Types[p]++
+		}
+	}
+}
+
+func (m Maze) SearchByScribble(scribble string) Scribble {
+	state := parseScribble(scribble)
+
+	for y := range m.Pixels {
+		if y+len(state.scribble) > len(m.Pixels) {
+			break
+		}
+		for x := range m.Pixels[y] {
+			if x+len(state.scribble[0]) > len(m.Pixels[y]) {
+				break
+			}
+
+			// start with match = true, bail if we find a mismatch
+			match := true
+			for sy := range state.scribble {
+				//fmt.Printf("Checking {%d, %d}\n", x, y)
+				for sx := range state.scribble[sy] {
+					//fmt.Printf("{%d, %d} %d == %d\n", x+sx, y+sy, m.Pixels[y+sy][x+sx], state.scribble[sy][sx])
+					if state.scribble[sy][sx] != 255 &&
+						state.scribble[sy][sx] != m.Pixels[y+sy][x+sx] {
+						//fmt.Println("no match!")
+						// fountain and chest look the same in a scribble
+						if !(state.scribble[sy][sx] == tFOUNTAIN &&
+							m.Pixels[y+sy][x+sx] == tCHEST) {
+							match = false
+							break
+						}
+
+					}
+				}
+			}
+
+			if match {
+				state.Matches = append(state.Matches, point{x, y})
+			}
+		}
+	}
+
+	return state
+}
+
+func (m Maze) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (m Maze) Bounds() image.Rectangle {
+	max := len(m.Pixels) * 5
+	return image.Rect(0, 0, max, max)
+}
+
+func (m Maze) At(x, y int) color.Color {
+	return mazeColorMap(m.Pixels[y/5][x/5])
+}
+
+func (m Maze) String() string {
+	return fmt.Sprintf("This map has %d chests, %d fountains and %d monsters", m.Types[tCHEST], m.Types[tFOUNTAIN], m.Types[tMONSTER])
+}
+
+func mazeColorMap(val uint8) color.Color {
+	switch val {
+	case tWALL:
+		return color.RGBA{0, 0, 0, 255}
+	case tPATH:
+		return color.RGBA{255, 255, 255, 255}
+	case tBONFIRE:
+		return color.RGBA{255, 165, 0, 255}
+	case tCHEST:
+		return color.RGBA{0, 255, 255, 255}
+	case tBOSS:
+		return color.RGBA{255, 0, 0, 255}
+	case tFOUNTAIN:
+		return color.RGBA{30, 180, 30, 255}
+	case tMONSTER:
+		return color.RGBA{140, 120, 255, 255}
+	case tFAMOUS:
+		return color.RGBA{0, 0, 255, 255}
+	default:
+		return color.RGBA{251, 4, 253, 255}
+	}
+}
+
+type point struct {
+	X int
+	Y int
+}
+
+type Scribble struct {
+	scribble       [][]uint8
+	playerLocation point
+	Matches        []point
+}
+
+func (s Scribble) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (s Scribble) Bounds() image.Rectangle {
+	ymax := len(s.scribble) * 5
+	xmax := len(s.scribble[0]) * 5
+	return image.Rect(0, 0, xmax, ymax)
+}
+
+func (s Scribble) At(x, y int) color.Color {
+	return mazeColorMap(s.scribble[y/5][x/5])
+}
+
+func (s Scribble) String() string {
+	return fmt.Sprintf("Found %d matches: %s", len(s.Matches), fmt.Sprint(s.Matches))
+}
+
+// parse a scribble string from chat wars
+func parseScribble(scribble string) Scribble {
+	rows := strings.Split(scribble, "\n")
+	var playerLocation point
+
+	scrib := make([][]uint8, len(rows))
+	for i := range rows {
+		// Remove Unicode Variation Selectors
+		realString := strings.Map(func(r rune) rune {
+			if unicode.In(r, unicode.Variation_Selector) {
+				return -1
+			}
+			return r
+		}, rows[i])
+		runes := []rune(realString)
+		scrib[i] = make([]uint8, len(runes))
+		fmt.Println(len(runes))
+		fmt.Println(realString)
+		for j, rune := range runes {
+			fmt.Printf("%d: \"%c\" %+q\n", j, rune, rune)
+			switch rune {
+			case '\u2b1b':
+				scrib[i][j] = tWALL
+			case '\u2b1c':
+				scrib[i][j] = tPATH
+			case '\U0001f7e8':
+				scrib[i][j] = 255 // player overwrites other types
+				playerLocation = point{j, i}
+			case '\U0001f7e9':
+				scrib[i][j] = tFOUNTAIN
+			case '\U0001f7e7':
+				scrib[i][j] = tBONFIRE
+			case '\U0001f7e6':
+				scrib[i][j] = tFAMOUS
+			case '\U0001f7ea':
+				scrib[i][j] = tMONSTER
+			default:
+				scrib[i][j] = 255
+			}
+		}
+	}
+	return Scribble{scrib, playerLocation, make([]point, 0)}
+}
